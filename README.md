6009e6474d8d292627829bd62fd3246ced8cfa1a
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2024-06-30 01:07:28 +0200

    finished my website portfolio

---
+++ README.md
@@ -0,0 +1,32 @@
+# portfolio ##
+
+My name is Ganeth Seetsi, and i can showcase my technical abilities and services through software development, web development, technical programming and database.
+
+<!-- Responsive Personal Portfolio -->
+. Key Features.
+
+- Responsive personal portfolio website using HTML CSS and JavaScript.
+- Smooth scrolling in each section.
+- Including light or dark mode.
+- Sending email in the contact section.
+- All mobile devices are compatible, and the user interface is stylish and pleasant.
+
+<!-- created Todo App using Flutter Dart Language -->
+. Todo App
+
+- Todo App is a new Flutter project that requires new users to register an account. After creating an account, users can  login. They will be able to create, modify, and delete tasks.  
+
+<!-- created a Calculator App using HTML CSS JavaScript -->
+. Calculator App
+
+- A simple Calculator app with fundamental mathematical operators.
+
+<!-- created Notes App using HTML CSS JavaScript -->
+. Notes App
+
+- Notes is a simple text editor. It's ideal for composing brief text documents that you want to save as plain text. If you've never used Notepad before, you might be surprised how simple it is to use.
+
+<!-- created Weather App with HTML CSS JavaScript -->
+. Weather App
+
+- Weather is the state of the atmosphere, comprising temperature, air pressure, wind, humidity, cloud cover, rain.
---
+++ js/script_bundles.js
@@ -0,0 +1,124 @@
+// Like or Dislike Button
+ // Get references to the button and like count elements
+ document.addEventListener('DOMContentLoaded', () => {
+    const commentElements = document.querySelectorAll('.comment');
+    const projectElements = document.querySelectorAll('.project');
+  
+    commentElements.forEach((comment) => {
+      const likeButton = comment.querySelector('.like-button');
+      const likeCount = comment.querySelector('.like-count');
+     // Initialize like status from local storage or default to false (not liked)
+     // Initialize like count from local storage or default to 0
+      let liked = JSON.parse(localStorage.getItem(`liked_${comment.dataset.commentId}`)) || false;
+      let count = parseInt(localStorage.getItem(`likes_${comment.dataset.commentId}`)) || 0;
+    // Set the initial like count  and button state
+      likeCount.textContent = count;
+    
+     // Event listener for the like button
+      likeButton.addEventListener('click', () => {
+        // Toggle the liked state
+        liked = !liked;
+        // Increment or decrement the count
+        count = liked ? count + 1 : count - 1;
+  
+        likeCount.textContent = count;
+      
+  
+       // Storage the updated count and liked status in local storage 
+        localStorage.setItem(`likes_${comment.dataset.commentId}`, count);
+        localStorage.setItem(`liked_${comment.dataset.commentId}`, JSON.stringify(liked));
+      });
+    });
+    
+    projectElements.forEach((project) => {
+      const likeButton = project.querySelector('.like-button');
+      const likeCount = project.querySelector('.like-count');
+     // Initialize like status from local storage or default to false (not liked)
+     // Initialize like count from local storage or default to 0
+      let liked = JSON.parse(localStorage.getItem(`liked_${project.dataset.projectId}`)) || false;
+      let count = parseInt(localStorage.getItem(`likes_${project.dataset.projectId}`)) || 0;
+  
+      likeCount.textContent = count;
+  
+     // Event listener for the like button
+     likeButton.addEventListener('click', () => {
+      // Toggle the liked state
+      liked = !liked;
+      // Increment or decrement the count
+      count = liked ? count + 1 : count - 1;
+  
+      likeCount.textContent = count;
+  
+     // Storage the updated count and liked status in local storage 
+        localStorage.setItem(`likes_${project.dataset.projectId}`, count);
+        localStorage.setItem(`liked_${project.dataset.projectId}`, JSON.stringify(liked));
+      });
+    });
+  });
+
+  function join(date, options, separator) {
+    function format(option) {
+      let formatter = new Intl.DateTimeFormat('en-US', option);
+      return formatter.format(date);
+    }
+    return options.map(format).join(separator);
+  }
+  // Create a Date object for
+  const myDate = new Date();
+  const options = [{weekday: 'long'},{ day: 'numeric' }, { month: 'short' }, { year: 'numeric' }];
+  const formattedDate = join(myDate, options, ' ~ ');
+  const formattedDateTime = `${formattedDate}`;
+  
+  document.getElementById('year').innerText = myDate.getUTCFullYear();
+  document.getElementById('datetime').innerText = formattedDateTime;
+  
+   function datetimes(){
+    
+    // Create a Date object for example Fri, 12 Nov 2020
+  const projects = [
+    {
+      startDate: "2024-02-05 ",
+      endDate: "2024-02-19T01:25"
+    }, 
+    {
+        startDate: "2024-04-27 ".toUpperCase(), 
+        endDate: "2024-05-12T21:45".toUpperCase()
+    },
+    {
+      startDate: "2024-05-20".toUpperCase(),
+      endDate: "2024-06-08T18:00".toUpperCase()
+    },
+    {
+      startDate: "2023-12-29".toUpperCase(),
+      endDate: "2024-01-25T15:45".toUpperCase()
+    }
+
+    // Add more projects here
+  ];
+
+  projects.forEach((project, index) => {
+    function formatDateTime(date) {
+      const options = {
+      Year: "numeric",
+      Month: "2-digit",
+      Day: "2-digit",
+      Hour: "2-digit",
+      Minute: "2-digit",
+      Hour12: false, // Use 24-hour format
+      timeZone: "UTC", // Use UTC timezone
+      };
+      return date.toLocaleString("en-US", options);
+      }
+      
+    const startDateElement = document.getElementById(`start-date-${index + 1}`);
+    const endDateElement = document.getElementById(`end-date-${index + 1}`);
+    const startDate = new Date(project.startDate);
+    const endDate = new Date(project.endDate);
+    
+    startDateElement.innerText = `${ formatDateTime = startDate.toLocaleDateString()}`;
+    endDateElement.innerText = `${endDate.toLocaleDateString()} ${ formatDateTime = endDate.toLocaleTimeString()}`;
+
+  }); 
+}
+   setInterval(datetimes, 1000);
+   datetimes();
