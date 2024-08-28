5c6f8e17f2d6d4bac619f87515016018904b2c2f
Ganeth <83027242+Seetsi1997@users.noreply.github.com>
2024-08-29 00:24:44 +0200

    changed some

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+  navClose = document.getElementById("nav-close"),
+   splash = document.querySelector('.splash');
 
+document.addEventListener('DOMContentLoaded', (e)=>{
+  setTimeout(()=>{
+    splash.classList.add('display-none');
+  }, 3000);
+})
+ 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
@@ -133,30 +140,6 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   keyboard: true, */
 });
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove('active-link');
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
-
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById("header");
@@ -212,6 +195,17 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon())
   
 });
+
+// Function to open the popup
+function openPopupCertificates() {
+  document.getElementById("modal-certificates").style.display = "inline";
+}
+
+// Function to close the popup
+function closePopupCertificates() {
+  document.getElementById("modal-certificates").style.display = "none";
+}
+
 
 
 
